2ae87ec0530f05199e2696cfb35937c49a605dca
xuannga <nguyenx206@gmail.com>
2021-06-30 23:50:51 -0700

    added questions

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
 // TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+
 
 // TODO: Create an array of questions for user input
-const questions = [
-    
-];
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What do you want the title to be?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please give a description of your project',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.',
+      },
+      {
+        type: 'input',
+        name: 'license',
+        message: 'List your licenses',
+      },
+      {
+        type: 'input',
+        name: 'credit',
+        message: 'List your collaborators',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Go the extra mile and write tests for your application. Then provide examples on how to run them here.',
+      },
+      {
+        type: 'input',
+        name: 'questions',
+        message: 'How to reach you',
+      },
+    ]);
+  };
+  
+  
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
